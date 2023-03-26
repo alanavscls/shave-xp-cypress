c70b58d9059bcf1c17d0abee471b20cabaaae621
Alana Vasconcelos <alanamvd30@gmail.com>
2023-03-26 14:52:55 -0300

    Continuação dos testes na página de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,11 @@
 /// <reference types="cypress" />
 
+import loginPage from '../support/pages/login'
+import shaversPage from '../support/pages/shavers'
+
+
 describe("login", () => {
+
   context("quando submeto o formulário", () => {
     it("deve logar com sucesso", () => {
       const user = {
@@ -9,16 +14,11 @@ describe("login", () => {
         password: "teste123",
       };
 
-      cy.visit("http://localhost:3000/");
+      loginPage.submit(user.email, user.password)
+      shaversPage.header.userShouldBeLoggedIn(user.name)
+      
 
-      cy.get("input[placeholder$=email]").type(user.email);
-      cy.get("input[placeholder*=senha]").type(user.password);
-
-      cy.contains("button", "Entrar").click();
-
-      cy.get(".logged-user div a")
-        .should("be.visible")
-        .should("have.text", "Olá, " + user.name);
+      
     });
 
     it("Não deve logar com senha incorreta", () => {
@@ -28,20 +28,12 @@ describe("login", () => {
         password: "teste123456",
       };
 
-      cy.visit("http://localhost:3000/");
-
-      cy.get("input[placeholder$=email]").type(user.email);
-      cy.get("input[placeholder*=senha]").type(user.password);
-
-      cy.contains("button", "Entrar").click();
+      loginPage.submit(user.email, user.password)
 
       const message =
         "Ocorreu um erro ao fazer login, verifique suas credenciais.";
 
-      cy.get(".notice-container")
-        .should("be.visible")
-        .find(".error p")
-        .should("have.text", message);
+      loginPage.noticeShouldBe(message)
     });
 
     it("Não deve logar com email não cadastrado", () => {
@@ -51,20 +43,63 @@ describe("login", () => {
         password: "teste123456",
       };
 
-      cy.visit("http://localhost:3000/");
-
-      cy.get("input[placeholder$=email]").type(user.email);
-      cy.get("input[placeholder*=senha]").type(user.password);
-
-      cy.contains("button", "Entrar").click();
+      loginPage.submit(user.email, user.password)
 
       const message =
         "Ocorreu um erro ao fazer login, verifique suas credenciais.";
 
-      cy.get(".notice-container")
-        .should("be.visible")
-        .find(".error p")
-        .should("have.text", message);
+      loginPage.noticeShouldBe(message)
     });
-  });
-});
+
+    it('campos obrigatórios', ()=> {
+      loginPage.submit()
+      loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
+
+      
+    })
+  })
+
+  context('senha muito curta', ()=> {
+
+    const passwords = [
+      '1',
+      '12',
+      '123',
+      '1234',
+      '12345'
+    ]
+
+    passwords.forEach((p)=> {
+      it('não deve logar com senha ' + p, ()=> {
+        loginPage.submit('teste123@gmail.com', p)
+        loginPage.alertShouldBe('Pelo menos 6 caracteres')        
+      })
+    })
+  })
+
+  context('email no formato incorreto', ()=> {
+
+    const emails = [
+      'teste&gmail.com',
+      'teste.com.br',
+      '@gmail.com',
+      '@',
+      'teste@',
+      '123456789',
+      '@#!@#!#@!',
+      'xpto123'
+    ]
+
+    emails.forEach((e)=> {
+      it('E-mail' + e + 'não é válido!', ()=> {
+        loginPage.submit(e, 'teste123') 
+        loginPage.alertShouldBe('Informe um email válido')     
+      })
+    })
+  })
+
+
+
+
+
+})
