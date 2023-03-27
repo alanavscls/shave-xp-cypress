a0f7a4c2dd2deb278de7130f25995ce3eebfa9b1
Alana Vasconcelos <alanamvd30@gmail.com>
2023-03-26 23:52:33 -0300

    Implementando fixtures

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,24 +1,16 @@
 /// <reference types="cypress" />
 
-import loginPage from '../support/pages/login'
-import shaversPage from '../support/pages/shavers'
-
+import loginPage from "../support/pages/login";
+import shaversPage from "../support/pages/shavers";
+import data from "../fixtures/users-login.json";
 
 describe("login", () => {
-
   context("quando submeto o formulário", () => {
-    it("deve logar com sucesso", () => {
-      const user = {
-        name: "Teste",
-        email: "teste123@gmail.com",
-        password: "teste123",
-      };
+    it.only("deve logar com sucesso", () => {
+      const user = data;
 
-      loginPage.submit(user.email, user.password)
-      shaversPage.header.userShouldBeLoggedIn(user.name)
-      
-
-      
+      loginPage.submit(user.email, user.password);
+      shaversPage.header.userShouldBeLoggedIn(user.name);
     });
 
     it("Não deve logar com senha incorreta", () => {
@@ -28,12 +20,12 @@ describe("login", () => {
         password: "teste123456",
       };
 
-      loginPage.submit(user.email, user.password)
+      loginPage.submit(user.email, user.password);
 
       const message =
         "Ocorreu um erro ao fazer login, verifique suas credenciais.";
 
-      loginPage.noticeShouldBe(message)
+      loginPage.noticeShouldBe(message);
     });
 
     it("Não deve logar com email não cadastrado", () => {
@@ -43,63 +35,48 @@ describe("login", () => {
         password: "teste123456",
       };
 
-      loginPage.submit(user.email, user.password)
+      loginPage.submit(user.email, user.password);
 
       const message =
         "Ocorreu um erro ao fazer login, verifique suas credenciais.";
 
-      loginPage.noticeShouldBe(message)
+      loginPage.noticeShouldBe(message);
     });
 
-    it('campos obrigatórios', ()=> {
-      loginPage.submit()
-      loginPage.requiredFields('E-mail é obrigatório', 'Senha é obrigatória')
+    it("campos obrigatórios", () => {
+      loginPage.submit();
+      loginPage.requiredFields("E-mail é obrigatório", "Senha é obrigatória");
+    });
+  });
 
-      
-    })
-  })
+  context("senha muito curta", () => {
+    const passwords = ["1", "12", "123", "1234", "12345"];
 
-  context('senha muito curta', ()=> {
+    passwords.forEach((p) => {
+      it("não deve logar com senha " + p, () => {
+        loginPage.submit("teste123@gmail.com", p);
+        loginPage.alertShouldBe("Pelo menos 6 caracteres");
+      });
+    });
+  });
 
-    const passwords = [
-      '1',
-      '12',
-      '123',
-      '1234',
-      '12345'
-    ]
-
-    passwords.forEach((p)=> {
-      it('não deve logar com senha ' + p, ()=> {
-        loginPage.submit('teste123@gmail.com', p)
-        loginPage.alertShouldBe('Pelo menos 6 caracteres')        
-      })
-    })
-  })
-
-  context('email no formato incorreto', ()=> {
-
+  context("email no formato incorreto", () => {
     const emails = [
-      'teste&gmail.com',
-      'teste.com.br',
-      '@gmail.com',
-      '@',
-      'teste@',
-      '123456789',
-      '@#!@#!#@!',
-      'xpto123'
-    ]
+      "teste&gmail.com",
+      "teste.com.br",
+      "@gmail.com",
+      "@",
+      "teste@",
+      "123456789",
+      "@#!@#!#@!",
+      "xpto123",
+    ];
 
-    emails.forEach((e)=> {
-      it('E-mail' + e + 'não é válido!', ()=> {
-        loginPage.submit(e, 'teste123') 
-        loginPage.alertShouldBe('Informe um email válido')     
-      })
-    })
-  })
-
-
-
-
-
-})
+    emails.forEach((e) => {
+      it("E-mail" + e + "não é válido!", () => {
+        loginPage.submit(e, "teste123");
+        loginPage.alertShouldBe("Informe um email válido");
+      });
+    });
+  });
+});
