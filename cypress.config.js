ffdc5ae1aaee694e7d6e674c1b5c9f44e94abed3
Alana Vasconcelos <alanamvd30@gmail.com>
2023-04-02 22:10:05 -0300

    Modularizando Tasks

--- cypress.config.js
+++ cypress.config.js
@@ -1,36 +1,13 @@
 const { defineConfig } = require("cypress");
 
-const { Pool } = require("pg");
-
-const dbConfig = {
-  host: "motty.db.elephantsql.com",
-  user: "qaocsbsy",
-  password: "qE0YMVaYyvY1NSmVxJLZuwRNeWvw15sP",
-  database: "qaocsbsy",
-  port: 5432,
-};
+const { removeUser } = require("./cypress/support/tasks/database");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on("task", {
-        removeUser(email) {
-          return new Promise(function (resolve) {
-            const pool = new Pool(dbConfig);
-
-            pool.query(
-              "DELETE FROM users WHERE email = $1",
-              [email],
-              function (error, result) {
-                if (error) {
-                  throw error;
-                }
-                resolve({ sucess: result });
-              }
-            );
-          });
-        },
+        removeUser,
       });
     },
     viewportWidth: 1920,
